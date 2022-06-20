7169537b09d3e3aec8e66fdaa0020de5875bf51a
17Keith <kayomega7@gmail.com>
2022-06-20 16:13:11 +0300

    Small Changes to Task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         while count < x:
             print("{}".format(my_list[count]), end='')
-            count +=1
+            count += 1
         print()
         return count
     except IndexError:
