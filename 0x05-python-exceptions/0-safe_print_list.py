496aca25c73ae3c050bec8f3b501d7ba0d25ea14
17Keith <kayomega7@gmail.com>
2022-06-20 22:59:05 +0300

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from itertools import count
+
+
+def safe_print_list(my_list=[], x=0):
+
+    i = 0
+    while i < x:
+        try:
+            print(my_list[i], end='')
+        except IndexError:
+            break
+        else:
+            i += 1 
+    print ()
+    return i
