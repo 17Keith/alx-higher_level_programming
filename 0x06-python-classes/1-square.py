83f7916107428f29807353494bf265e9f94ffbce
17Keith <kayomega7@gmail.com>
2022-06-21 17:01:21 +0300

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is a class that defines a Square based on 0-square.py"""
+
+
+class Square(object):
+    """Class:Square"""
+    def __init__(self, size):
+        """Initialization of Class"""
+        self.__size = size
