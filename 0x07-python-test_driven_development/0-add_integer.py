c40e332cd950baff6b753ec96c8c637a8df73005
17Keith <kayomega7@gmail.com>
2022-06-27 17:19:13 +0300

    TDD

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is a function that adds 2 integers a and b=98 \
+    If both a and b are not integers, a TypeError is raised \
+        with exeption message 'a must be an integer or b must be an integer' \
+            If a and b are floats, they must be casted to integers.
+"""
+
+
+def add_integer(a, b=98):
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [int, float]:
+        raise TypeError ("b must be an integer")
+    return (int(a) + int(b))
