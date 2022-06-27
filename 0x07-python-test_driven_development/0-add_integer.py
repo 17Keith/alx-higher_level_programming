bd6a52040c27756365b5412bf76b15e47a870583
17Keith <kayomega7@gmail.com>
2022-06-27 17:26:02 +0300

    Fix Pycodestyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,5 +11,5 @@ def add_integer(a, b=98):
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
-        raise TypeError ("b must be an integer")
+        raise TypeError("b must be an integer")
     return (int(a) + int(b))
