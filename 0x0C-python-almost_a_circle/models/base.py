c5a326bb8a94236b1724423caeaa22a3e411b848
17Keith <kayomega7@gmail.com>
2022-07-11 21:48:25 +0300

    Circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class to be the base of all other classes in this projet"""
+
+class Base:
+    """Class with: Private class attribute: __nb_objects"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialization of a Base instance.
+        Args:
+            - id: id of the instance
+        """
+
+        if type(id) != int and id is not None:
+            raise TypeError("id must be an integer")
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
