20c4c1700eef15cd21b0cbc2fca2042116e8595b
17Keith <kayomega7@gmail.com>
2022-08-09 23:56:37 +0300

    SQL Task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,2 @@
 -- This script inserts a new row in a table 
-INSERT INTO
-    `fitst_table` (`id`, `name`)
-VALUES
-    (89, 'Best School');
+INSERT INTO `fitst_table` (`id`, `name`) VALUES (89, 'Best School');
