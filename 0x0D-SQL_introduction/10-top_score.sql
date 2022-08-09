047ec106a0a252de9868069658e8f73be345ae87
17Keith <kayomega7@gmail.com>
2022-08-10 00:33:44 +0300

    SQL Task 10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,11 @@
 -- This script lists all records of a table
+-- Lists all the tables of a database.
+-- score is ordered from top to bottom
+-- Displays the name and the score
+SELECT
+    score,
+    name
+FROM
+    second_table
+ORDER BY
+    score DESC;
