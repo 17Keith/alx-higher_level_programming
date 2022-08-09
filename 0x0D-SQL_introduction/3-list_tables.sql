e6f438424d23a832729735efd17acedb0f2dc7af
17Keith <kayomega7@gmail.com>
2022-08-10 00:24:33 +0300

    SQL Task 10

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,10 @@
 -- Lists all the tables of a database.
-SHOW TABLES; 
+-- score is ordered from top to bottom
+-- Displays the name and the score
+SELECT
+    score,
+    name
+FROM
+    second_table
+ORDER BY
+    score DESC;
