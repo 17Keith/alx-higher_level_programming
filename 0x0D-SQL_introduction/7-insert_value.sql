1f794c615517fb130be21ba12738423c2c0975a5
17Keith <kayomega7@gmail.com>
2022-08-10 00:02:14 +0300

    SQL Task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- This script inserts a new row in a table 
-INSERT INTO `fitst_table` (`id`, `name`) VALUES (89, 'Best School');
+-- A script that inserts a new role in a table. 
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
