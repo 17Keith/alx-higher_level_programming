d2dcb4c83be0c03b60e53f0b062b0f59687afe7d
17Keith <kayomega7@gmail.com>
2022-09-23 21:52:48 +0300

    P-Net-ADV

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Prints a list of the first 10 commits from recent to oldest"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    reponame = argv[1]
+    ownername = argv[2]
+
+    r = requests.get(
+        'https://api.github.com/repos/{}/{}/commits'.format(reponame, ownername))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        for i in r.json()[:10]:
+            print("{}: {}".format(i.get('sha'),
+                                  i.get('commit').get('author').get('name')))
