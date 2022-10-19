bfff89da5aab149a8454ca4a8f15823703c83f8f
17Keith <kayomega7@gmail.com>
2022-10-19 03:34:33 +0300

    Js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,10 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/'+ process.argv[2], function (error,  body) {
+request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, _response, body) {
   if (error) {
-    console.log(error);
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
   }
-  console.log(JSON.parse(body).title);
 });
